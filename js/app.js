26e9e1474e6165c0efee06310d45d88196f729f8
ingridsag <starlling@hotmail.com>
2018-09-19 21:16:01 -0500

    subiendo repo con avances

---
+++ js/app.js
@@ -0,0 +1,20 @@
+let button = document.getElementById('abrir_modal');
+let modal = document.getElementById('mi_modal');
+let modalContainer = document.getElementById("container");
+let close = document.getElementById("close");
+
+button.addEventListener('click', function(){
+    modal.style.display = 'block';
+});
+
+close.addEventListener ('click', function(){
+    modal.style.display = 'none';
+});
+
+//https://www.youtube.com/watch?v=YonqjrU2btI
+//minuto 23:58 falta esconder el modal cuándo das click fuera del mismo
+window.addEventListener ('click', function(e) {
+   if (e.target  == modal) {
+    modal.style.display = 'none';
+   }
+});
